2caa02b9f2a3d674abbaa761bed3d8ae464388d2
Ravindu Lachiththa <88020270+ravindusj@users.noreply.github.com>
2025-04-04 01:47:13 +0530

    Added main components to run

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,101 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.nsbm.studyx"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+     packagingOptions {
+         jniLibs {
+            useLegacyPackaging = true
+            pickFirsts.add("lib/**/libflutter.so")
+            pickFirsts.add("lib/**/libapp.so")
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.nsbm.studyx"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        
+        
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
+
+    lint {
+        disable += "InvalidPackage"
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+flutter {
+    source = "../.."
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+// Add a configuration to exclude firebase-iid
+configurations.all {
+    resolutionStrategy {
+        // Force specific version of firebase-messaging
+        force("com.google.firebase:firebase-messaging:24.1.0")
+        // Exclude the problematic firebase-iid
+        exclude(group = "com.google.firebase", module = "firebase-iid")
+    }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.annotation:annotation:1.7.1")
+    
+    // Explicitly declare firebase-messaging and exclude firebase-iid
+    implementation("com.google.firebase:firebase-messaging:24.1.0") {
+        exclude(group = "com.google.firebase", module = "firebase-iid")
+    }
+    
+    // Make sure other Firebase dependencies also exclude firebase-iid
+    implementation("com.google.firebase:firebase-firestore-ktx") {
+        exclude(group = "com.google.firebase", module = "firebase-iid")
+    }
+    
+    implementation("com.google.firebase:firebase-storage-ktx") {
+        exclude(group = "com.google.firebase", module = "firebase-iid")
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,194 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:provider/provider.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart'; 
+import 'package:flutter/foundation.dart' show kDebugMode;
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'canteen_page.dart';
+import 'chat_page.dart';
+import 'auth_page.dart';
+import 'main_app_scaffold.dart';
+import 'theme.dart';
+import 'notes_page.dart'; 
+import 'dashboard_page.dart'; 
+import 'settings_page.dart';
+import 'providers/theme_provider.dart';
+import 'ai_tools_page.dart';
+import 'todo_page.dart';
+import 'meditation_page.dart';
+import 'services/notification_service.dart';
+import 'notification_test_page.dart';
+import 'progress_tracker_page.dart';
+import 'services/study_session_service.dart';
+import 'services/progress_notification_service.dart';
+import 'services/audio_service.dart';
+import 'math_solver_page.dart';
+import 'deadline_tracker_page.dart';
+import 'services/deadline_service.dart';
 
-void main() {
-  runApp(const MyApp());
+
+final GlobalKey<MainAppScaffoldState> mainAppScaffoldKey = GlobalKey<MainAppScaffoldState>();
+
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  try {
+
+    await Firebase.initializeApp();
+    debugPrint('Handling a background message: ${message.messageId}');
+  } catch (e) {
+    debugPrint('Error handling background message: $e');
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  try {
+    await Firebase.initializeApp();
+    
+    try {
+      FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    } catch (e) {
+      debugPrint('Error setting background message handler: $e');
+    }
+    
+    try {
+      await NotificationService().initialize();
+      await ProgressNotificationService().initialize();
+    } catch (e) {
+      debugPrint('Error initializing notification services: $e');
+    }
+  } catch (e) {
+    debugPrint('Error initializing Firebase: $e');
+  }
+  
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => StudySessionService()),
+        ChangeNotifierProvider(create: (_) => AudioService()),
+        ChangeNotifierProvider(create: (_) => DeadlineService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'StudyX',
+      debugShowCheckedModeBanner: false,
+      themeMode: themeProvider.themeMode,
+      theme: AppTheme.lightTheme(),
+      darkTheme: AppTheme.darkTheme(),
+      home: const AuthenticationWrapper(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Show splash screen while waiting
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SplashScreen();
+        }
+
+        // User is logged in
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        }
+        
+        // User is not logged in
+        return const AuthPage();
+      },
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).colorScheme.primary;
+    final accentColor = Theme.of(context).colorScheme.tertiary;
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 120,
+              height: 120,
+              errorBuilder: (context, error, stackTrace) => Icon(
+                Icons.school,
+                size: 80,
+                color: primaryColor,
+              ),
+            ),
+            const SizedBox(height: 24),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'StudyX',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+            const SizedBox(height: 16),
+            LoadingAnimationWidget.stretchedDots(
+              color: isDarkMode ? accentColor : primaryColor,
+              size: 50,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final studySessionService = Provider.of<StudySessionService>(context);
+    
+
+    final List<Widget> screens = [
+      const DashboardPage(),
+      const NotesPage(),
+      const TodoPage(),
+      const ProgressTrackerPage(),
+      const DeadlineTrackerPage(),
+      const MeditationPage(),
+      const ChatPage(),
+      const CanteenPage(),
+      const MathSolverPage(),
+      const AiToolsPage(),
+      const SettingsPage(),
+      if (kDebugMode) const NotificationTestPage(),
+    ];
+    
+    
+    return MainAppScaffold(
+      key: mainAppScaffoldKey,
+      screens: screens,
     );
   }
 }
